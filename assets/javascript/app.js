dc93ec2c83613119901a128dc882a2d71cb4718c
thatlostboy <thatlostboy@gmail.com>
2018-08-02 23:09:29 -0700

    Trying authnetication with github again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,24 +16,35 @@ $(document).ready(function () {
 
     // Initialize instance of github provider object
     var provider = new firebase.auth.GithubAuthProvider();
-
-    // // sign in with popup
-    // firebase.auth().signInWithPopup(provider).then(function(result) {
-    //     // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-    //     var token = result.credential.accessToken;
-    //     // The signed-in user info.
-    //     var user = result.user;
-    //     // ...
-    //   }).catch(function(error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     var email = error.email;
-    //     // The firebase.auth.AuthCredential type that was used.
-    //     var credential = error.credential;
-    //     // ...
-    //   });
+/*
+    // redirect function
+    firebase.auth().getRedirectResult().then(function (result) {
+        if (result.credential) {
+            // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+            var token = result.credential.accessToken;
+            // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+*/
+    // check if user is logged, if not, redirect to login page
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          console.log("user is logged in: ", user);
+        } else {
+            firebase.auth().signInWithRedirect(provider);
+        }
+      });
 
 
     // initialize Time for banner, add train, and modals.  
