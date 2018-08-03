b3ccb54c2d20ca89835cad8f4735d1805b20ef46
thatlostboy <thatlostboy@gmail.com>
2018-08-02 23:45:48 -0700

    app.js testin authenticatoin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,6 +29,28 @@ $(document).ready(function () {
     });
 
 
+    // get redirect result
+    firebase.auth().getRedirectResult().then(function (result) {
+        if (result.credential) {
+            // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+            var token = result.credential.accessToken;
+            // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+        console.log("redirectresults", user);
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+
+
     // initialize Time for banner, add train, and modals.  
     localTime = moment();
     $("#currentTime").text(localTime.format("MM/DD/YYYY hh:mm A"));
